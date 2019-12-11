7845a2f5f532e66b9ee9efba7ef219a8a95a98c5
root <root@ip-172-31-11-36.ap-south-1.compute.internal>
2019-12-11 16:39:10 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!! i am back
+          I just deployed full flow to Docker!!
         </p>
         <a
           className="App-link"
